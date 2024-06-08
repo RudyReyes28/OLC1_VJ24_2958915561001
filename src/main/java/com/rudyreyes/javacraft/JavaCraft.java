c3963fe5cb338b2bf9af73a551602fa042a25a5a
Rudy Reyes <rudyreyes202031213@cunoc.edu.gt>
2024-06-07 23:12:19 -0600

    interfaz grafica

---
+++ src/main/java/com/rudyreyes/javacraft/JavaCraft.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rudyreyes.javacraft;
+
+/**
+ *
+ * @author rudyo
+ */
+public class JavaCraft {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
